243524d9dfeb934cadaef51ed7323834363e42e7
Maxwell Faria <maxwell.faria@riocardmais.com.br>
2026-06-04 13:56:38 -0300

    Update versão.

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ if (fs.existsSync(envPath)) {
 module.exports = {
   name: "Meu Álbum Completo",
   slug: "figurinha",
-  version: "1.0.4",
+  version: "1.0.5",
   orientation: "portrait",
   backgroundColor: "#0B0F19",
   icon: "./assets/icon.png",
@@ -39,7 +39,7 @@ module.exports = {
     },
     package: "com.figurinha.copa2026",
     permissions: ["android.permission.CAMERA"],
-    versionCode: 8,
+    versionCode: 9,
   },
   plugins: [
     [
